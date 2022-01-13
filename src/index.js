1eb961b77cb18ac03815e89393ea48c61893d8d7
Cristina Sanchez García <cristinasanchezgarcia@MacBook-Pro-de-Cristina.local>
2022-01-13 18:11:43 +0100

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const element = (
+  <div>
+    Hola mundo!
+  </div>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
